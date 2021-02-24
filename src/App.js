5b8247e3860da1e8237c94c8d2f472c7ab9342e0
rajesh dhiman <rajeshdhimanskt@gmail.com>
2021-02-24 22:18:38 +0530

    created theme colors and added sample elements for reference

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App  bg-sp-white">
+      <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12">
+       
+        <div className="relative bg-sp-section-white py-3 sm:max-w-xl sm:mx-auto">
+        <h2 className="text-sp-heading-blue"> Section Heading</h2>
+          <div className="relative px-4 py-10  sm:rounded-3xl sm:p-20 flex space-x-4">
+            <button className="bg-sp-btn-default hover:bg-sp-btn-default-dark text-white font-bold py-2 px-4 rounded">Default</button>
+          
+            <button className="bg-sp-btn-selected hover:bg-sp-btn-selected-dark text-white font-bold py-2 px-4 rounded">Selected</button>
+         
+            <button className="bg-sp-btn-primary hover:bg-sp-btn-primary-dark text-white font-bold py-2 px-4 rounded">Primary</button>
+          </div>
+          <h3 className="text-sp-text-highlight">Some Text to highlight</h3>
+        <p className="text-sp-text-default">Regular texts like product description</p>
+        </div>
+        <div>
+
+        </div>
+      </div>
     </div>
   );
 }
