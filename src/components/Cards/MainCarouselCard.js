16aee4aa351ea6717714577247a4b2c6ebc813ef
Imagine-Me <prince100thomas@gmail.com>
2021-02-27 19:14:15 +0530

    Add translation for carousel text

--- src/components/Cards/MainCarouselCard.js
+++ src/components/Cards/MainCarouselCard.js
@@ -1,4 +1,4 @@
-
+import { FormattedMessage } from 'react-intl'
 
 export default function MainCarouselCard({ title, description, image }) {
     console.log(image);
@@ -6,14 +6,14 @@ export default function MainCarouselCard({ title, description, image }) {
         <div className="absolute inset-0 p-3 flex items-center lg:justify-center lg:relative lg:w-2/4 lg:p-0">
             <div className="w-9/12 md:w-2/4">
                 <div className="text-gray-500 text-xl">
-                    {title}
-                </div>  
-                <div className="text-5xl lg:text-6xl">
-                    {description}
+                    <FormattedMessage id={title} />
+                </div>
+                <div className="text-5xl mt-5 lg:text-6xl">
+                    <FormattedMessage id={description} />
                 </div>
             </div>
         </div>
-        <div className="w-full bg-top bg-cover bg-no-repeat lg:w-2/4" style={{backgroundImage: `url(${process.env.REACT_APP_BASE_URL}/${image})`}}>
+        <div className="w-full bg-top bg-cover bg-no-repeat lg:w-2/4" style={{ backgroundImage: `url(${process.env.REACT_APP_BASE_URL}/${image})` }}>
 
         </div>
     </div>
