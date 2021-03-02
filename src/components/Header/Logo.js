ba309fb283acc215ebf74f1ca18b15b5acee02c1
rajesh dhiman <rajeshdhimanskt@gmail.com>
2021-03-02 13:02:59 +0530

    updated application title font size and added logo

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -2,8 +2,9 @@ import { Link } from "react-router-dom";
 
 function Logo() {
   return <Link to="/"
-    className="text-l font-bold leading-relaxed inline-block py-2 whitespace-no-wrap uppercase text-sp-heading-blue"
+    className="text-2xl font-bold leading-relaxed py-2 whitespace-no-wrap uppercase text-sp-heading-blue flex items-center"
   >
+    <img className="w-6 h-6 rounded-full" src="/logo.png" alt="shopay logo" />
     shopay
     </Link>
 }
