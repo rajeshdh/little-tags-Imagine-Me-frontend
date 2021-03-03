3394b21430e6c40c8e4554ff755d80bc55568eab
rajesh dhiman <rajeshdhimanskt@gmail.com>
2021-03-03 16:14:05 +0530

    refactored login template

---
+++ src/IconSet/EmailIcon.js
@@ -0,0 +1,3 @@
+export const EmailIcon = () => <svg className="h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+<path d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+</svg>
--- src/IconSet/index.js
+++ src/IconSet/index.js
@@ -5,3 +5,8 @@ export {Close} from "./Close";
 export {Open} from "./Open";
 export {LoginIcon} from "./LoginIcon";
 export {CartIcon} from "./CartIcon";
+export {ArrowRound} from "./ArrowRound";
+export {EmailIcon} from "./EmailIcon";
+export {PasswordIcon} from "./PasswordIcon";
+export {SignUpIcon} from "./SignUpIcon";
+
