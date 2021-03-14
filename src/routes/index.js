00218af34f3b4a3223a90451f5770320a2746955
rajesh dhiman <rajeshdhimanskt@gmail.com>
2021-03-15 01:42:31 +0530

    fix route changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,39 +3,36 @@ import { Switch } from "react-router-dom";
 import Home from "../pages/Home/Index";
 import Route from "./Route";
 
-const Login = lazy(() => import("../pages/Login"))
-const Register = lazy(() => import("../pages/Register"))
-const Checkout = lazy(() => import("../pages/Checkout"))
-const Payment = lazy(() => import("../pages/Checkout/PaymentSelection"))
-const ProductPage = lazy(() => import("../pages/ProductList/Index"))
-const SearchPage = lazy(() => import("../pages/Search"))
-const Product = lazy(() => import("../pages/Product"))
-const Cart = lazy(() => import("../pages/Cart"))
-const WishList = lazy(() => import("../pages/Wishlist"))
-const OrderHistory = lazy(() => import("../pages/OrderHistory"))
-const ThankYou = lazy(() => import("../pages/Checkout/ThankYou"))
-
+const Login = lazy(() => import("../pages/Login"));
+const Register = lazy(() => import("../pages/Register"));
+const Checkout = lazy(() => import("../pages/Checkout"));
+const Payment = lazy(() => import("../pages/Checkout/PaymentSelection"));
+const ProductPage = lazy(() => import("../pages/ProductList/Index"));
+const SearchPage = lazy(() => import("../pages/Search"));
+const Product = lazy(() => import("../pages/Product"));
+const Cart = lazy(() => import("../pages/Cart"));
+const WishList = lazy(() => import("../pages/Wishlist"));
+const OrderHistory = lazy(() => import("../pages/OrderHistory"));
+const ThankYou = lazy(() => import("../pages/Checkout/ThankYou"));
 
 const Routes = () => {
-	return (
-		<Switch>
-			<Route exact path="/" component={Home} />
-			<Route exact path="/login" isLoginRoute component={Login} />
-			<Route exact path="/register" isLoginRoute component={Register} />
-			{/* <Route exact path="/checkout" isPrivate component={Checkout} /> */}
-			<Route exact path="/checkout"  component={Checkout} />
-				<Route exact path="/payment"  component={Payment} />
-				<Route exact path="/thankyou"  component={ThankYou} />
-			<Route exact path="/cart" isPrivate component={Cart} />
-			<Route exact path="/wishlist" isPrivate component={WishList} />
-			<Route exact path="/order-history" isPrivate component={OrderHistory} />
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" isLoginRoute component={Login} />
+      <Route exact path="/register" isLoginRoute component={Register} />
+      <Route exact path="/checkout" isPrivate component={Checkout} />
+      <Route exact path="/payment" isPrivate component={Payment} />
+      <Route exact path="/thankyou" isPrivate component={ThankYou} />
+      <Route exact path="/cart" isPrivate component={Cart} />
+      <Route exact path="/wishlist" isPrivate component={WishList} />
+      <Route exact path="/order-history" isPrivate component={OrderHistory} />
 
-			<Route exact path="/search" component={SearchPage} />
-			<Route exact path="/product/:productid" component={Product} />
-			<Route exact path="/:type/:keyWord" component={ProductPage} />
-
-		</Switch>
-	);
+      <Route exact path="/search" component={SearchPage} />
+      <Route exact path="/product/:productid" component={Product} />
+      <Route exact path="/:type/:keyWord" component={ProductPage} />
+    </Switch>
+  );
 };
 
 export default Routes;
