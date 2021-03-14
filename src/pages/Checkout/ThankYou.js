97b71e112550c861d7e46889e67a9f185e4fb58d
rajesh dhiman <rajeshdhimanskt@gmail.com>
2021-03-15 01:03:45 +0530

    add translations for thank you page

--- src/pages/Checkout/ThankYou.js
+++ src/pages/Checkout/ThankYou.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FormattedMessage } from "react-intl";
 
 function ThankYou() {
   return (
@@ -7,13 +8,13 @@ function ThankYou() {
        
         <div class="text-center lg:w-2/3 w-full">
           <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            Thank You.
+          <FormattedMessage id="thankyou" defaultMessage="Thank You." />
           </h1>
           <p class="mb-8 leading-relaxed text-lg">
-            Your order was completed successfully.
+          <FormattedMessage id="orderPlaced" defaultMessage="Your order was placed successfully."/>
           </p>
           <p class="mb-8 leading-relaxed">
-          An email receipt including the details about your order has been sent to the email address provided. Please keep it for your records.
+          <FormattedMessage id="emailSent" defaultMessage="An email receipt including the details about your order has been sent to the email address provided. Please keep it for your records." />
           </p>
         </div>
       </div>
